f9b05a127283b2e78b7daaa568e6cff8dd3bd586
DESKTOP-F5G7RUH\bandiyesol <yesolpark0909@gmail.com>
2025-04-07 15:48:38 +0900

    02_weeks

---
+++ 02주차_실습.py
@@ -0,0 +1,34 @@
+import sys
+
+
+
+
+# S = sys.stdin.readline().strip()
+# print(S[0] +"학년 "+ S[1] + S[2] +"반 "+ S[3]+"번")
+
+
+
+# S = sys.stdin.readline().strip()
+# print(S +"의 차량번호 4자리는 " + S[len(S)-1] + S[len(S)-2] + S[len(S)-3] + S[len(S)-4] + "입니다.")
+
+
+
+# S = sys.stdin.readline().strip()
+# if len(S) % 2 == 1:
+# 	print(S + "의 가운데 글자는 " + S[len(S)//2] + "입니다.")
+# else:
+# 	print(S + "의 가운데 글자는 " + S[len(S)//2-1] + S[len(S)//2] + "입니다.")
+
+
+
+# L = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+# print(L[2:7])
+# print(L[2:7][1])
+
+
+
+# dic = {"금요일" : "탕수육", "토요일" : "유산슬"}
+# dic["일요일"] = "팔보채"
+# for key in dic.keys():
+# 	value = dic[key]
+# 	print(f"{key} : {value}")
